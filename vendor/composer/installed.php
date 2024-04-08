0bc9bc55467b375f88473eda44899e1f8853947f
trdmonkey <trdmonkey218@gmail.com>
2024-04-08 15:11:29 -0500

    Iniciando proyecto en composer agregando libreria nusoap

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => null,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => null,
+            'dev_requirement' => false,
+        ),
+        'econea/nusoap' => array(
+            'pretty_version' => 'v0.9.17',
+            'version' => '0.9.17.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../econea/nusoap',
+            'aliases' => array(),
+            'reference' => 'ac2322f37808ecb1a2b84ed469cba9c51648eb8d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
